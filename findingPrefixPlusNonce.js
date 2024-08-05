0f8b90ecdd5f5d8b0e80b6c99317b5993675c3a7
AimalKhanOfficial <Aimalkhanofficial@gmail.com>
2024-08-05 19:46:24 -0400

    finding prefix + nonce

--- findingPrefixPlusNonce.js
+++ findingPrefixPlusNonce.js
@@ -1,5 +1,25 @@
 let crypto = require('crypto');
 
+//find a nonce/target that has a prefix "aimalPrefix"
+let findPrefixWithNonce = (target) => {
+    let input = 596139; //why 596139? cuz 596139 was '00000'
+    let result = '';
+    let inputStr = '';
+    while (!result.startsWith(target)) {
+        inputStr = 'aimalPrefix' + input.toString(); //now this is '00000' + the prefix
+        result = crypto.createHash('sha256').update(result).digest('hex');
+        input++;
+    }
+    console.log('>>> input', input);
+    return {
+        inputStr,
+        result
+    }
+}
+
+console.log('>> findPrefix thats 5 zeros i.e. 00000', findPrefixWithNonce('00000'));
+
+//works fine - find the number that starts with 5 zeros
 let findPrefix = (target) => {
     let input = 0;
     let result = '';
@@ -9,5 +29,3 @@ let findPrefix = (target) => {
     }
     return {result, input};
 }
-
-console.log('>> findPrefix thats 5 zeros i.e. 00000', findPrefix('00000'));
